9ade3ba85e6fae63600ffa406ed7363e7b401201
shamilmyran <97163572+shamilmyran@users.noreply.github.com>
2026-03-09 12:40:43 +0530

    New version

--- bot/translate-bot/src/config.js
+++ bot/translate-bot/src/config.js
@@ -1,19 +1,16 @@
 const tgbot=require("node-telegram-bot-api")
 let bot,token;
-const adminId = 1591775154
+const adminId = 1591775154 // YOUR TELEGRAM ID
+
 if(process.env.NODE_ENV==="production"){
-  
-  token = "1869324755:AAEYjYu3b5cbg1M7VrCJUzmk5HnuPHUAxaA"
+  token = process.env.TRANSLATE_BOT_TOKEN || "1869324755:AAEYjYu3b5cbg1M7VrCJUzmk5HnuPHUAxaA" // REPLACE WITH YOUR NEW BOT TOKEN
   bot=new tgbot(token)
   bot.stopPolling()
   bot.setWebHook(process.env.HEROKU_URL+"/translate/"+token)
 }else{
   console.log("translate bot working locally");
-  
- // token = "1869324755:AAEYjYu3b5cbg1M7VrCJUzmk5HnuPHUAxaA"
   token = "5367698671:AAETky8B52O2sC278lZWgCnz0foEH9YkIyg"
   bot=new tgbot(token)
-  //bot.stopPolling().catch(e=>console.log(e.body))
   bot.startPolling().catch(e=>console.log(e.body))
 }
 
