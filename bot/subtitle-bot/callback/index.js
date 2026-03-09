52ed38a45b928b61b86e644fb5812b7230c7a63a
shamilmyran <97163572+shamilmyran@users.noreply.github.com>
2026-03-09 16:45:16 +0530

    Update index.js

--- bot/subtitle-bot/callback/index.js
+++ bot/subtitle-bot/callback/index.js
@@ -1,35 +1,29 @@
+const { bot } = require("../../config");
+const { sendDocument, answerCallback } = require("../src/message");
+const got = require("got");
+const fs = require("fs");
+const encode = require("detect-file-encoding-and-language");
+const encoder = require("encoding");
 
-const {bot,root}=require("../src/config"),
-{sendDocument, answerCallback:answer}= require("../src/message")
-
-const got=require("got"),
-fs=require("fs"),
-encode=require("detect-file-encoding-and-language"),
-encoder=require("encoding")
-
-bot.on("callback_query",async msg=>{
+bot.on("callback_query", async msg => {
   try {
-    let callback=JSON.parse(msg.data);
-    if(callback.t!=="fix_enc")return
-    
-    let chat=msg.from.id
-    
-    let url=await bot.getFileLink(msg.message.document.file_id)
-    let file= await got.get(url,{responseType:"buffer"})
-    await fs.writeFileSync(root+"/subs/"+chat+".sub",file.body)
-    let {language,encoding}=await encode(root+"/subs/"+chat+".sub")
-   
-    //console.log(decode(file.body.toString()));
-    file.body=encoder.convert(file.body,"ISO-8859-1")
-    await fs.writeFileSync(root+"/subs/"+chat+".sub",file.body)
-    await sendDocument(chat,root+"/subs/"+chat+".sub",{caption:msg.message.caption.replace(/If you face .+$/,"")},{filename:msg.message.document.file_name})
-    //await fs.unlinkSync(root+"/subs/"+chat+".sub")
+    let callback = JSON.parse(msg.data);
+    if (callback.t !== "fix_enc") return;
+
+    let chat = msg.from.id;
+    let url = await bot.getFileLink(msg.message.document.file_id);
+    let file = await got.get(url, { responseType: "buffer" });
+    let filePath = process.cwd() + "/bot/subtitle-bot/subs/" + chat + ".sub";
+    await fs.writeFileSync(filePath, file.body);
+    let { encoding } = await encode(filePath).catch(() => ({ encoding: "UTF-8" }));
+    if (encoding !== "UTF-8") {
+      file.body = encoder.convert(file.body, "UTF-8", encoding);
+      await fs.writeFileSync(filePath, file.body);
+    }
+    await sendDocument(chat, filePath, { caption: msg.message.caption.replace(/If you face .+$/, "") }, { filename: msg.message.document.file_name });
+    await fs.unlinkSync(filePath);
   } catch (e) {
     console.log(e);
-    if(typeof e!==String)e="Unexpected Error Ocuured"
-    return answer(msg.id,e)
+    answerCallback(msg.id, "Unexpected error");
   }
-})
-
-
-
+});
